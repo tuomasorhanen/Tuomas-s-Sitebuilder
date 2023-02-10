e4d44a29a270af70ff4ba07bdb7da37088cfa1a4
Tuomas <tuomas.orhanen@amban.fi>
2023-02-10 17:03:49 +0200

    add flowbite

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -3,7 +3,12 @@ const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './sanity/**/*.{ts,tsx}'],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './sanity/**/*.{ts,tsx}',
+    './node_modules/flowbite/**/*.js',
+  ],
   theme: {
     ...theme,
     colors: {
@@ -32,5 +37,5 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
