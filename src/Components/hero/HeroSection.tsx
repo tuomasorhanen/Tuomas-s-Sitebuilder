2e26e9a7dc5388b7dd37967222105c38d0a0e05d
Tuomas <tuomas.orhanen@amban.fi>
2023-02-10 14:06:14 +0200

    refuctor css on HeroSection - "slash right"

--- src/Components/hero/HeroSection.tsx
+++ src/Components/hero/HeroSection.tsx
@@ -12,7 +12,7 @@ const HeroSection = (props: IHero) => {
   switch (layout) {
     case 'slash-right':
       return (
-        <div key={props._key} className="relative bg-gray-900">
+        <div key={props._key} className="relative">
           <div className="mx-auto max-w-7xl">
             <div className="relative z-10 lg:w-full lg:max-w-2xl">
               <svg
@@ -22,41 +22,38 @@ const HeroSection = (props: IHero) => {
                 aria-hidden="true">
                 <polygon points="0,0 90,0 50,100 0,100" />
               </svg>
-              <div className="relative py-32 px-6 sm:py-40 lg:py-56 lg:px-8 lg:pr-0">
-                <div className="mx-auto max-w-2xl text-center lg:mx-0 lg:max-w-xl lg:text-start">
-                  <h1 className="text-4xl font-bold tracking-tight text-gray-100 sm:text-6xl">{title}</h1>
-                  <p className="mt-6 text-lg leading-8 text-gray-100">{description}</p>
-
-                  <div className="flex justify-center lg:justify-start">
-                    <div className="mt-10 flex items-center gap-x-6">
-                      {buttons &&
-                        buttons.map(btn => {
-                          return (
-                            <div key={btn.navigateToPage} className="flex justify-center lg:justify-start">
-                              <div className="mt-10 flex items-center gap-x-6">
-                                <Link
-                                  href={btn.navigateToPage}
-                                  className="rounded-md bg-gray-100 px-3.5 py-1.5 text-base font-semibold leading-7 text-gray-900 shadow-sm hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-800">
-                                  {btn.callToAction}
-                                </Link>
-                              </div>
-                            </div>
-                          );
-                        })}
+              <div className="relative py-32 px-6 sm:py-40 lg:py-56">
+                <div className="text-center">
+                  <h1 className="text-6xl font-bold">{title}</h1>
+                  <p className="mt-4">{description}</p>
+                  {buttons && (
+                    <div className="mt-10 flex justify-center">
+                      {buttons.map(btn => {
+                        return (
+                          <div key={btn.navigateToPage} className="">
+                            <Link
+                              href={btn.navigateToPage}
+                              className="mx-2 rounded-md border-2 p-2 shadow-xl hover:scale-105">
+                              {btn.callToAction}
+                            </Link>
+                          </div>
+                        );
+                      })}
                     </div>
-                  </div>
+                  )}
                 </div>
               </div>
             </div>
           </div>
-          <div className="bg-gray-900 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-            <Img {...imageProps} className="hidden aspect-[6/2] w-full object-cover  lg:block lg:h-full" alt="" />
+          <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+            <Img {...imageProps} className="hidden aspect-[6/2]  object-cover lg:block lg:h-full" alt="" />
           </div>
         </div>
       );
+
     case 'circle-left':
       return (
-        <div key={props._key} className="bg-white pb-16 lg:relative lg:z-10 lg:pb-0">
+        <div key={props._key} className="pb-16 lg:relative lg:z-10 lg:pb-0">
           <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-8 lg:px-8">
             <div className="relative lg:-my-8">
               <div aria-hidden="true" className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden" />
@@ -83,7 +80,7 @@ const HeroSection = (props: IHero) => {
                             <div className="mt-10 flex items-center gap-x-6">
                               <Link
                                 href={btn.navigateToPage}
-                                className="rounded-md border-2 border-gray-400 bg-gray-100 px-3.5 py-1.5 text-base font-semibold leading-7 text-gray-900 shadow-xl hover:scale-110">
+                                className="rounded-md border-2 p-2 shadow-xl hover:scale-105">
                                 {btn.callToAction}
                               </Link>
                             </div>
