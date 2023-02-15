0b160d09ec37a4d212a6f2a8b8b6f6ee8635459a
Tuomas <tuomas.orhanen@amban.fi>
2023-02-15 09:52:46 +0200

    add link to referenceblogsection

--- src/components/blog/BlogReferenceSection.tsx
+++ src/components/blog/BlogReferenceSection.tsx
@@ -2,11 +2,12 @@ import { client } from '_lib/client';
 import { IBlog, IReference } from '_lib/types';
 import Img from 'next/image';
 import { useNextSanityImage } from 'next-sanity-image';
+import Link from 'next/link';
 
 type IBlogSectionProps = IBlog & IReference & {};
 
 const BlogReferenceSection = (props: IBlogSectionProps) => {
-  const { excerpt, image, category, readingTime, title, author } = props;
+  const { excerpt, image, category, readingTime, title, author, slug } = props;
 
   const imageProps = useNextSanityImage(client, image);
 
@@ -14,18 +15,20 @@ const BlogReferenceSection = (props: IBlogSectionProps) => {
     <div key={props._key} className="px-4">
       <div className="rounded-lg border-2 bg-gray-100">
         <Img {...imageProps} className="h-48 w-full rounded-t-lg object-cover" alt="" />
-        <div className="m-4 p-2 text-black">
-          <h2 className="text-3xl font-bold">{title}</h2>
-          <p className="mt-4">{excerpt}</p>
-          <div className="mt-4">
-            <p className="text-sm ">{author}</p>
-            <div className=" text-sm text-gray-500">
-              <span>
-                {readingTime} &middot; {category}
-              </span>
+        <Link href={`/blog/${slug.current}`}>
+          <div className="m-4 p-2 text-black">
+            <h2 className="text-3xl font-bold">{title}</h2>
+            <p className="mt-4">{excerpt}</p>
+            <div className="mt-4">
+              <p className="text-sm ">{author}</p>
+              <div className=" text-sm text-gray-500">
+                <span>
+                  {readingTime} &middot; {category}
+                </span>
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
     </div>
   );
