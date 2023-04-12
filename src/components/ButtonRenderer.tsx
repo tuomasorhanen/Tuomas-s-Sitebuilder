1d62c1588be65dc451478e80f391054da1c69527
Tuomas <tuomas.orhanen@amban.fi>
2023-04-12 18:21:36 +0300

    fix key issue

--- src/components/ButtonRenderer.tsx
+++ src/components/ButtonRenderer.tsx
@@ -12,21 +12,21 @@ const ButtonRenderer = (props: ICallToAction) => {
 
   if (linkType === 'internal') {
     return (
-      <Link key={_key} href={navigateToPage || '/home'}>
+      <Link key={props._key} href={navigateToPage || '/home'}>
         {image ? (
           <Image {...image} alt="" className={`h-16 w-16 object-cover mx-2`} />
         ) : (
-          <span key={_key} className={`button`} style={buttonStyle}>{callToAction}</span>
+          <span key={props._key} className={`button`} style={buttonStyle}>{callToAction}</span>
         )}
       </Link>
     );
   } else if (linkType === 'external') {
     return (
-      <a key={_key} href={navigateToUrl} style={buttonStyle}>
+      <a key={props._key} href={navigateToUrl} style={buttonStyle}>
         {image ? (
           <Image {...image} alt="" className={`h-16 w-16 object-cover mx-2`} />
         ) : (
-          <span key={_key} className={`button`} style={buttonStyle}>{callToAction}</span>
+          <span key={props._key} className={`button`} style={buttonStyle}>{callToAction}</span>
         )}
       </a>
     );
